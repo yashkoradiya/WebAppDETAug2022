3f230a7f65377aad0480f6b7336896c5177b189b
yashkoradiya <yashkoradiya55@gmail.com>
2022-09-13 14:55:30 +0530

    hiii

--- WebAppDETAug2022/Services/IPLEventServices.cs
+++ WebAppDETAug2022/Services/IPLEventServices.cs
@@ -2,47 +2,46 @@
 using WebAppDETAug2022.Models;
 
 namespace WebAppDETAug2022.Services
-{
-   
+{ 
     public static class PizzaService
+{
+    static List<Pizza> Pizzas { get; }
+    static int nextId = 3;
+    static PizzaService()
     {
-        static List<Pizza> Pizzas { get; }
-        static int nextId = 3;
-        static PizzaService()
-        {
-            Pizzas = new List<Pizza>
+        Pizzas = new List<Pizza>
                 {
                     new Pizza { Id = 1, Name = "Classic Italian", Price=20.00M, Size=PizzaSize.    Large, IsGlutenFree = false },
                     new Pizza { Id = 2, Name = "Veggie", Price=15.00M, Size=PizzaSize.Small,     IsGlutenFree = true }
                 };
-        }
+    }
 
-        public static List<Pizza> GetAll() => Pizzas;
+    public static List<Pizza> GetAll() => Pizzas;
 
-        public static Pizza? Get(int id) => Pizzas.FirstOrDefault(p => p.Id == id);
+    public static Pizza? Get(int id) => Pizzas.FirstOrDefault(p => p.Id == id);
 
-        public static void Add(Pizza pizza)
-        {
-            pizza.Id = nextId++;
-            Pizzas.Add(pizza);
-        }
+    public static void Add(Pizza pizza)
+    {
+        pizza.Id = nextId++;
+        Pizzas.Add(pizza);
+    }
 
-        public static void Delete(int id)
-        {
-            var pizza = Get(id);
-            if (pizza is null)
-                return;
+    public static void Delete(int id)
+    {
+        var pizza = Get(id);
+        if (pizza is null)
+            return;
 
-            Pizzas.Remove(pizza);
-        }
+        Pizzas.Remove(pizza);
+    }
 
-        public static void Update(Pizza pizza)
-        {
-            var index = Pizzas.FindIndex(p => p.Id == pizza.Id);
-            if (index == -1)
-                return;
+    public static void Update(Pizza pizza)
+    {
+        var index = Pizzas.FindIndex(p => p.Id == pizza.Id);
+        if (index == -1)
+            return;
 
-            Pizzas[index] = pizza;
+        Pizzas[index] = pizza;
         }
     }
 }
